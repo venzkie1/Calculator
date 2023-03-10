6ef71449e639367fd53f2c502a22806459fc06b0
KevinMembreve <kevinmembreve32@gmail.com>
2023-03-11 00:18:14 +0800

    first commit

---
+++ js/calculator.js
@@ -0,0 +1,95 @@
+function clk(val)
+{
+    document.getElementById(`input`).value=document.getElementById(`input`).value+val;
+}
+
+function clrdisp()
+{
+    document.getElementById(`input`).value = ""
+    document.getElementById(`output`).value = ""
+}
+
+function getOperation(textArray) {
+    const operationChar = ["+","-","*","/","%"]
+    for(let i = 0; i < textArray.length; i++) {
+        if (operationChar.includes(textArray[i])) {
+            return i;
+        }
+    }
+}
+
+function compute(fistNumber, secondNumber, operation) {
+    let computation = 0;
+    if (isNaN(fistNumber) || isNaN(secondNumber)) return
+    switch (operation) {
+      case '+':
+        computation = fistNumber + secondNumber;
+        break
+      case '-':
+        computation = fistNumber - secondNumber;
+        break
+      case '*':
+        computation = fistNumber * secondNumber;
+        break
+      case '/':
+        computation = fistNumber / secondNumber;
+        break
+        case '%':
+        computation = fistNumber / secondNumber;
+        break
+      default:
+        return
+    }
+    return computation;
+}
+
+function equal(){
+    const text=document.getElementById("input").value;
+    console.log('text', text);
+    const splittedText = text.split("");
+    console.log('splittedText', splittedText);
+    const operationIdx = getOperation(splittedText);
+    console.log('operationIdx', operationIdx);
+    const operation = splittedText[operationIdx];
+    console.log('operation', operation);
+    const splittedNumber = text.split(`${operation}`);
+    const firstNumber = parseFloat(splittedNumber[0]);
+    const secondNumber = parseFloat(splittedNumber[1]);
+    
+    console.log('firstNumber', firstNumber);
+    console.log('operation',operation );
+    console.log('secondNumber', secondNumber);
+    let result = compute(firstNumber, secondNumber, operation);
+    console.log('result', result);
+    document.getElementById("output").value=result;
+
+}
+
+let myCalculator = document.getElementById(`calculator`);
+myCalculator.style.display = `none`;
+function displayCalculator()
+{
+    if(myCalculator.style.display ===`none`)
+    {
+        myCalculator.style.display = `block`;
+    }
+    else{
+        myCalculator.style.display = "none"
+    }
+}
+
+
+// let a;
+// function displayCalculator()
+// {
+//     if (a==1)
+//     {
+//         document.getElementById("calculator").style.display="inline";
+//         return a=0;
+//     }
+//     else
+//     {
+//         document.getElementById("calculator").style.display="none";
+//         return a=1;
+//     }
+// }
