9e929e8ebe9d48f5804ad13bc16e358d594f6488
KevinMembreve <kevinmembreve32@gmail.com>
2023-03-12 00:43:22 +0800

    added modulo

--- js/calculator.js
+++ js/calculator.js
@@ -10,8 +10,10 @@ function clrdisp()
 }
 
 function getOperation(textArray) {
+  // array for opperands
     const operationChar = ["+","-","*","/","%"]
     for(let i = 0; i < textArray.length; i++) {
+      // includes method is to search if the choosen array item is in the array list
         if (operationChar.includes(textArray[i])) {
             return i;
         }
@@ -34,8 +36,9 @@ function compute(fistNumber, secondNumber, operation) {
       case '/':
         computation = fistNumber / secondNumber;
         break
-        case '%':
-        computation = fistNumber / secondNumber;
+        // remainder of divided two numbers
+      case '%':
+        computation = fistNumber % secondNumber;
         break
       default:
         return
@@ -44,23 +47,51 @@ function compute(fistNumber, secondNumber, operation) {
 }
 
 function equal(){
+  // to make const text hold the inputted value
     const text=document.getElementById("input").value;
+    // checking to see if I was able to get the value
     console.log('text', text);
+
+    // To split the inputted value and make it into an array
     const splittedText = text.split("");
+    // Checking to see the splitted value
     console.log('splittedText', splittedText);
+
+    // To get the opperand from the array list from splittedText
+    // 5 + 5 = operationIdx 1. 5 being the 0, + being the one and so on.
     const operationIdx = getOperation(splittedText);
+    // Checking to see if I was able to get the value
     console.log('operationIdx', operationIdx);
+
+    // To take the array item from the array list from operationIdx
     const operation = splittedText[operationIdx];
+    // Checking if I was able to get the value
     console.log('operation', operation);
+
+    // split method to create a sub array for inputted value in text and not including the operation
+    // [value1, value2]
     const splittedNumber = text.split(`${operation}`);
+    console.log('splittedNumber', splittedNumber);
+
+    // Making the splitted numbers into a number
+    //   [0]      [1]
+    // [value1, value2]
     const firstNumber = parseFloat(splittedNumber[0]);
     const secondNumber = parseFloat(splittedNumber[1]);
     
+    // Checking to see if I got the values of firstNumber, secondNumber and operation.
     console.log('firstNumber', firstNumber);
     console.log('operation',operation );
     console.log('secondNumber', secondNumber);
+
+    // variable result to hold the result of computation
+    // function compute is called to loop through different conditions in switch case.
     let result = compute(firstNumber, secondNumber, operation);
+
+    // Checking the result
     console.log('result', result);
+
+    // Display the output
     document.getElementById("output").value=result;
 
 }
@@ -92,6 +123,10 @@ function displayCalculator() {
     button.textContent = "Use Calculator";
   }
 }
+
+
+
+
 // let a;
 // function displayCalculator()
 // {
